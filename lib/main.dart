03448f7325940f370153ae8a0b19dd7c2150dd80
Touy_1104 <83419012+Touy2004@users.noreply.github.com>
2025-10-03 18:30:00 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firsebase/auth_service.dart';
+import 'package:firsebase/bucket_service.dart';
+import 'package:firsebase/home_page.dart';
+import 'package:firsebase/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => AuthService()),
+        ChangeNotifierProvider(create: (context) => BucketService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final user = context.read<AuthService>().currentUser();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
+      home: user == null ? const LoginPage() : const HomePage(),
+    );
+  }
+}
